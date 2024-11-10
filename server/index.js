f67ca09e9c6791268a3413e22484114773b0edc8
Sabit205 <130175838+Sabit205@users.noreply.github.com>
2024-11-10 22:17:59 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,37 +3,30 @@ const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
-const cookieParser = require('cookie-parser')
-const { initializeSocket } = require('./socket/index')
+const cookiesParser = require('cookie-parser')
+const { app, server } = require('./socket/index')
 
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-
+// const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin : process.env.FRONTEND_URL,
+    credentials : true
 }))
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', (request, response) => {
+app.get('/',(request,response)=>{
     response.json({
-        message: "Server running at " + PORT
+        message : "Server running at " + PORT
     })
 })
 
-// API endpoints
-app.use('/api', router)
+//api endpoints
+app.use('/api',router)
 
-// Initialize socket connection
-initializeSocket(server)
-
-// Connect to MongoDB and start the server
-connectDB().then(() => {
-    server.listen(PORT, () => {
-        console.log("Server running at " + PORT)
+connectDB().then(()=>{
+    server.listen(PORT,()=>{
+        console.log("server running at " + PORT)
     })
 })
